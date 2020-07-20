abba647f2202610e15abf4879b5881cf6efcf7a3
Unknown <fioremartina@hotmail.com>
2020-07-20 18:07:32 +0200

    still bad.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all'inizio il software richiede anche una difficoltà all'utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
 //Lista vuota per numeri pc
 var numeri = [];
 var randomnum;
@@ -27,31 +40,18 @@ while ( sceltaR.length < 5 ) {
     console.log("Numero già inserito, scegline un altro");
   } else {
     sceltaR.push(scelta);
-    if (numeri.includes(scelta)) {
+    if ( (numeri.includes(scelta) == false) && (sceltaR.length < 5) ) {
+      console.log( sceltaR.length + " " + "Salvo! Puoi continuare");
+    } else if (numeri.includes(scelta) == true) {
       console.log( scelta + " " + "Bomba! Hai perso");
       break;
     } else {
-      console.log("Salvo! Puoi continuare");
+      console.log("Hai vinto!");
     }
   }
-
   i++;
 }
 console.log(sceltaR);
-
-
-
-//BOHH
-// while ( (numeri.includes(sceltaR[i]) == false) && (sceltaR.length < 5) ) {
-//   console.log("Puoi continuare");
-// } if (numeri.includes(sceltaR[i]) == true) {
-//   console.log("Bomba! Hai perso");
-// }
-
-
-// else if (i > 5) {
-//   console.log("Hai vinto!");
-// }
 
 
 // FUNZIONI
